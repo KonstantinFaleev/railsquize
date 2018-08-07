ff31efb770b4783ca51f67927a765117981ba6d1
KonstantinFaleev <konstantin.faleev@gmail.com>
2018-08-07 16:21:03 +0300

    Init model choice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_131656) do
+ActiveRecord::Schema.define(version: 2018_08_07_131937) do
+
+  create_table "choices", force: :cascade do |t|
+    t.string "text"
+    t.boolean "correct"
+    t.integer "question_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
